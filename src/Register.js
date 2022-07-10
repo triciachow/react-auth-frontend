f05b0f4aff7a7c098375008701c291d9eccd6975
triciachow0212 <suyeechow0212@gmail.com>
2022-07-10 16:32:12 +0800

    Set state for register component

--- src/Register.js
+++ src/Register.js
@@ -1,19 +1,41 @@
+import { useState } from "react";
 import { Button, Form } from "react-bootstrap";
 
 export default function Register() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [register, setRegister] = useState(false);
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    alert("submitted");
+  };
+
   return (
     <>
       <h2>Register</h2>
-      <Form>
+      <Form onSubmit={e => handleSubmit(e)}>
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Email Address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Control
+            type="email"
+            name="email"
+            value={email}
+            placeholder="Enter email"
+            onChange={e => setEmail(e.target.value)}
+          />
         </Form.Group>
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
-          <Form.Control type="text" placeholder="********" />
+          <Form.Control
+            type="text"
+            name="password"
+            value={password}
+            placeholder="********"
+            onChange={e => setPassword(e.target.value)}
+          />
         </Form.Group>
-        <Button variant="primary" type="submit">
+        <Button variant="primary" type="submit" onClick={e => handleSubmit(e)}>
           Submit
         </Button>
       </Form>
