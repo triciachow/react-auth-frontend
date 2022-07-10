a4c472a50adef93aa6042da032f03dda437d154c
triciachow0212 <suyeechow0212@gmail.com>
2022-07-10 15:02:15 +0800

    Link Login component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Col, Container, Row } from "react-bootstrap";
 import Register from "./Register";
+import Login from "./Login";
 
 function App() {
   return (
@@ -8,7 +9,9 @@ function App() {
         <Col md={6} lg={6}>
           <Register />
         </Col>
-        <Col md={6} lg={6}></Col>
+        <Col md={6} lg={6}>
+          <Login />
+        </Col>
       </Row>
     </Container>
   );
