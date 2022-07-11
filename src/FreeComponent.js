bb4464753f4fe4534ffe5582b84068c5881a6e03
triciachow0212 <suyeechow0212@gmail.com>
2022-07-11 17:13:36 +0800

    Call free end-point

--- src/FreeComponent.js
+++ src/FreeComponent.js
@@ -1,7 +1,30 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
 export default function FreeComponent() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    const configuration = {
+      method: "get",
+      url: "https://auth-app-node-mongodb.herokuapp.com/free-endpoint",
+    };
+
+    axios(configuration)
+      .then(result => {
+        setMessage(result.data.message);
+      })
+      .catch(error => {
+        error = new Error();
+      });
+  }, []);
+
   return (
     <>
       <div>FreeComponent</div>
+      <p className="alert-success p-2 border border-success rounded">
+        {message}
+      </p>
     </>
   );
 }
